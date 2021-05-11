2e8c6b8fc07558556cb090754e84e2ebac3db153
CodingNav <navleenkaurr@gmail.com>
2021-05-10 23:15:19 -0400

    Added Rellax to second svg

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,21 +1,21 @@
-$("#svg-1").load("candy-background.html", function() {
-    var rellax = new Rellax('.rellax', {
-        speed: -2,
-        center: false,
-        wrapper: null,
-        round: true,
-        vertical: true,
-        horizontal: false
-      });
+$("#svg-1").load("candy-background.html", function () {
+  var rellax = new Rellax('.rellax', {
+    speed: -2,
+    center: false,
+    wrapper: null,
+    round: true,
+    vertical: true,
+    horizontal: false
+  });
 });
 
-$("#svg-2").load("night-background.html", function() {
-    var rellax = new Rellax('.rellax', {
-        speed: -2,
-        center: false,
-        wrapper: null,
-        round: true,
-        vertical: true,
-        horizontal: false
-      });
+$("#svg-2").load("night-background.html", function () {
+  var rellax = new Rellax('.night-rellax', {
+    speed: -2,
+    center: false,
+    wrapper: null,
+    round: true,
+    vertical: true,
+    horizontal: false
+  });
 });
