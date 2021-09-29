3d1f4833395cb9d704c51715ed5be95b32fabe20
Navleen <navleenkaurr@gmail.com>
2021-09-28 20:11:15 -0400

    Set up cards to load through javascript

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,3 +1,62 @@
+var carouselCell;
+
+for(i = 0; i < projects.length; i++) {
+  var card = `
+  <!-- CARD -->
+    <div class="card mb-2">
+      <!-- CAROUSEL -->
+      <div id="carouselExampleControls${i}" class="card-img-top carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+        ${projects[i].images.map((imageURL, index) => {
+          if(index == 0) {
+            return `<div class="carousel-item active">
+                      <img class="d-block w-100" src="${imageURL}" alt="Second slide">
+                    </div>`
+          }
+          else {
+            return `<div class="carousel-item">
+                      <img class="d-block w-100" src="${imageURL}" alt="Second slide">
+                    </div>`
+          }
+        }).join("")}
+          <a class="carousel-control-prev" href="#carouselExampleControls${i}" role="button"
+            data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleControls${i}" role="button"
+            data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+      </div>
+      <!-- BODY -->
+      <div class="card-body">
+        <h2 class="card-title">${projects[i].title}</h2>
+        <p class="card-text">${projects[i].description}</p>
+        <p class="card-text">${projects[i].technologies}</p>
+      </div>
+      <footer class="card-footer">
+        <a class="effect effect-5" href="${projects[i].url}" target="_blank"
+          title="Live"><i class="fas fa-eye"></i> LIVE</a>
+        <a class="effect effect-5" href="${projects[i].repo}" target="_blank"
+          title="Code"><i class="fas fa-code"></i> CODE</a>
+      </footer>
+    </div>
+  `;
+
+  if (i%2 == 0) {
+    carouselCell = $(`<div class="carousel-cell card-deck"></div>`);
+  }
+
+  carouselCell.append(card);
+  $(".desktop.main-carousel").append(carouselCell);
+
+  $(".mobile.main-carousel").append(`<div class="carousel-cell card-deck">${card}</div>`);
+}
+
+
 $("#svg-1").load("candy-background.html", function () {
   var rellax = new Rellax('.rellax', {
     speed: -2,
@@ -55,8 +114,4 @@ $("#contact-form").submit(function (event) {
       $("#contact-form").html("<p class='text-center'>Sorry, the message was not sent. You can email me directly at navleenkaurr@gmail.com</p>")
       // console.log('FAILED...', error);
     });
-
-})
-
-
-
+});
---
+++ assets/javascript/projects.js
@@ -0,0 +1,48 @@
+var projects = [
+    {
+        images: ["./assets/images/pirate_mockup.png", "./assets/images/game_screen.gif"],
+        title: "Pirate RPG",
+        description: `Pirate RPG is a fighting/strategy game based on a war between the pirates.
+        The goal is to conquer the other pirates by defeating their toughest fighters. If attacks
+        aren't planned accordingly then your crew may wind up on the losing end.`,
+        technologies: "HTML, CSS, Javascript",
+        url: "https://codingnav.github.io/Pirate-RPG/",
+        repo: "https://github.com/CodingNav/Pirate-RPG#readme"
+    },
+    {
+        images: ["./assets/images/urban_mockup.png"],
+        title: "Urban Fashion",
+        description: `Urban Fashion is a retail website designed to sell trendy winter clothes`,
+        technologies: "HTML, CSS",
+        url: "https://codingnav.github.io/Urban-Fashion/",
+        repo: "https://github.com/CodingNav/Urban-Fashion#readme"
+    },
+    {
+        images: ["./assets/images/course_finder_mockup.png", "./assets/images/course_finder.gif"],
+        title: "Course Finder",
+        description: `Course finder is a web application with a back-end API service that
+        provides students access to information about the courses offered at St John's University.`,
+        technologies: "HTML, CSS, Javascript",
+        url: "https://sju-course-finder.netlify.app/",
+        repo: "https://github.com/CodingNav/Course-Search#readme"
+    },
+    {
+        images: ["./assets/images/spa_quiz_mockup.png", "../assets/images/spa_quiz.gif"],
+        title: "Web Development Quiz",
+        description: `Web Development Quiz is a set of two beginner practice quizzes for upcoming
+        web developers.`,
+        technologies: "HTML, CSS, Javascript",
+        url: "https://spa-quiz.netlify.app/",
+        repo: "https://github.com/CodingNav/SPA-Quiz#readme"
+    },
+    {
+        images: ["./assets/images/crystal_collector.png", "./assets/images/crystal_collector.gif"],
+        title: "Crystal Collector",
+        description: `Crystal Collector is a basic math game revolving around addition. The goal
+        is to add hidden values through clicking the crystals to reach the target number that's
+        given. If you go over the target value, you lose.`,
+        technologies: "HTML, CSS, Javascript",
+        url: "https://codingnav.github.io/Crystal-Collector-Game//",
+        repo: "https://github.com/CodingNav/Crystal-Collector-Game#readme"
+    }
+]
