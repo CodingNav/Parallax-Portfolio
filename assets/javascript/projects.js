91b2a2e5003c5b70ea2eea19300336b084e8c8c6
Navleen <navleenkaurr@gmail.com>
2021-09-28 21:17:07 -0400

    Added new projects to portfolio

--- assets/javascript/projects.js
+++ assets/javascript/projects.js
@@ -1,13 +1,47 @@
 var projects = [
     {
+        images: ["./assets/images/mahal_mockup.png"],
+        title: "Mahal",
+        description: `Mahal is a website for an Indian restaurant in New York.`,
+        technologies: "HTML, CSS, Javascript",
+        url: "https://mahalny.com",
+        repo: "https://github.com/CodingNav/Mahal-Restaurant"
+    },
+    {
+        images: ["./assets/images/gurdwara_mockup.png", "./assets/images/gurdwara_about.png"],
+        title: "Gurdwara Guru Tegh Bahadur Sahib",
+        description: `This is a website for the New York Sikh Temple.`,
+        technologies: "HTML, CSS, Javascript",
+        url: "https://www.gurdwarateghbahadur.com/",
+        repo: "https://github.com/CodingNav/Hillside-Gurdwara"
+    },
+    {
         images: ["./assets/images/pirate_mockup.png", "./assets/images/game_screen.gif"],
         title: "Pirate RPG",
         description: `Pirate RPG is a fighting/strategy game based on a war between the pirates.
         The goal is to conquer the other pirates by defeating their toughest fighters. If attacks
         aren't planned accordingly then your crew may wind up on the losing end.`,
-        technologies: "HTML, CSS, Javascript",
+        technologies: "HTML, CSS, Javascript, JQuery",
         url: "https://codingnav.github.io/Pirate-RPG/",
         repo: "https://github.com/CodingNav/Pirate-RPG#readme"
+    },
+    {
+        images: ["./assets/images/weather_mockup.png"],
+        title: "Weather Dashboard",
+        description: `Weather Dashboard allows travelers to see the weather outlook for multiple 
+        cities so that they can plan a trip accordingly.`,
+        technologies: "HTML, CSS, Materialize, Javascript, JQuery, Moment.js",
+        url: "https://codingnav.github.io/Weather-Dashboard/",
+        repo: "https://github.com/CodingNav/Weather-Dashboard#readme"
+    },
+    {
+        images: ["./assets/images/code_quiz_mockup.png"],
+        title: "Code Quiz",
+        description: `This responsive Code Quiz Challenge is a timed quiz on Bootstrap fundamentals 
+        that stores high scores so that you can gauge your progress compared to your peers`,
+        technologies: "HTML, CSS, Javascript",
+        url: "https://codingnav.github.io/Code-Quiz/",
+        repo: "https://github.com/CodingNav/Code-Quiz#readme"
     },
     {
         images: ["./assets/images/urban_mockup.png"],
@@ -18,11 +52,31 @@ var projects = [
         repo: "https://github.com/CodingNav/Urban-Fashion#readme"
     },
     {
+        images: ["./assets/images/team_mockup.png", "./assets/images/team_terminal.png", "./assets/images/team_test.png"],
+        title: "Team Profile Generator",
+        description: `Team Profile Generator is a Node.js command-line application that takes in information 
+        about employees on a software engineering team, then generates an HTML webpage that displays summaries for 
+        each person. Testing is key to making code maintainable, so I wrote a unit test for every part of the code and 
+        ensured that it passed each test.`,
+        technologies: "HTML, CSS, Bootstrap, Javascript, Nodejs, Inquirer, Jest",
+        url: "#",
+        repo: "https://github.com/CodingNav/Team-Profile-Generator#readme"
+    },
+    {
+        images: ["./assets/images/readme_terminal.png", "./assets/images/readme_mockup.png"],
+        title: "README Generator",
+        description: `README Generator is a command-line application that accepts user input and generates a 
+        professional README for a new project.`,
+        technologies: "Javascript, Nodejs",
+        url: "#",
+        repo: "https://github.com/CodingNav/README-Generator#readme"
+    }, 
+    {
         images: ["./assets/images/course_finder_mockup.png", "./assets/images/course_finder.gif"],
         title: "Course Finder",
         description: `Course finder is a web application with a back-end API service that
         provides students access to information about the courses offered at St John's University.`,
-        technologies: "HTML, CSS, Javascript",
+        technologies: "HTML, CSS, Bootstrap, Javascript, JQuery, Nodejs, Expressjs",
         url: "https://sju-course-finder.netlify.app/",
         repo: "https://github.com/CodingNav/Course-Search#readme"
     },
@@ -31,7 +85,7 @@ var projects = [
         title: "Web Development Quiz",
         description: `Web Development Quiz is a set of two beginner practice quizzes for upcoming
         web developers.`,
-        technologies: "HTML, CSS, Javascript",
+        technologies: "HTML, CSS, Bootstrap, Javascript, Handlebars.js",
         url: "https://spa-quiz.netlify.app/",
         repo: "https://github.com/CodingNav/SPA-Quiz#readme"
     },
@@ -41,8 +95,35 @@ var projects = [
         description: `Crystal Collector is a basic math game revolving around addition. The goal
         is to add hidden values through clicking the crystals to reach the target number that's
         given. If you go over the target value, you lose.`,
-        technologies: "HTML, CSS, Javascript",
-        url: "https://codingnav.github.io/Crystal-Collector-Game//",
+        technologies: "HTML, CSS, Bootstrap, Javascript, JQuery",
+        url: "https://codingnav.github.io/Crystal-Collector-Game/",
         repo: "https://github.com/CodingNav/Crystal-Collector-Game#readme"
+    },
+    {
+        images: ["./assets/images/note_taker-mockup.png"],
+        title: "Note Taker",
+        description: `Note Taker is an application that uses an Express.js back end to allow the user to write and saves notes.`,
+        technologies: "HTML, CSS, Bootstrap, Javascript, Nodejs, Expressjs",
+        url: "https://codingnav-note-taker.herokuapp.com/",
+        repo: "https://github.com/CodingNav/Note-Taker#readme"
+    },
+    {
+        images: ["./assets/images/day_planner_mockup.png"],
+        title: "Day Planner",
+        description: `Day Planner is a calendar application that allows a user to save events for each 
+        hour of the day. This will help the user stay more organized for their work day. This app runs in 
+        the browser and feature dynamically updated HTML and CSS powered by jQuery.`,
+        technologies: "HTML, CSS, Bootstrap, Javascript, JQuery",
+        url: "https://codingnav.github.io/Day-Planner/",
+        repo: "https://github.com/CodingNav/Day-Planner#readme"
+    },
+    {
+        images: ["./assets/images/refactor_mockup.png"],
+        title: "Code Refactor",
+        description: `Code Refactor is a web application made for Horiseon, a marketing agency. 
+        It follows accessibility standards, making it optimized for search engines.`,
+        technologies: "HTML, CSS, Javascript",
+        url: "https://codingnav.github.io/Horiseon-Refactor/",
+        repo: "https://github.com/CodingNav/Horiseon-Refactor#readme"
     }
 ]
